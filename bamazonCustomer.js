82be5be5d69ada9c01cb9caae67ac1d7f0be7385
Emily Tucker <emilytucker4@yahoo.com>
2019-10-01 17:35:12 -0400

    added the the connection info

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,27 @@
 var mysql = require ("mysql");
 var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "bamazon"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    start();
+  });
+
+  function start(){
+      inquirer
+        .prompt({
+            type:"input",
+            message:"Welcome, to bamazon here's our list of items what would like to purchase?",
+            name:"products"
+        })
+        .then(function(answer){
+            
+        })
+  }
