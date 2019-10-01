adc5e0d3b01b4d8721d4ebcb9b9dd8bbe7733616
Emily Tucker <emilytucker4@yahoo.com>
2019-10-01 16:35:34 -0400

    finished adding each product for the 'store'

--- bamazon_db.sql
+++ bamazon_db.sql
@@ -15,20 +15,20 @@ PRIMARY KEY (item_id)
 INSERT INTO products(product_name, department_name, price, stock_quantity)
 	    VALUES("Dog Dragon Costume", "Pet", 12.99, 50);
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-	    VALUES();
+	    VALUES("Cat Litter", "Pet", 17.13, 15);
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-	    VALUES();
+	    VALUES("Seat Cover", "Automotive", 24.69, 75);
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-	    VALUES();
+	    VALUES("Replacement LED Lights Pack of 20", "Automotive", 5.99, 100);
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-	    VALUES();
+	    VALUES("Digital Food Thermometer", "Kitchen", 13.32, 20);
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-	    VALUES();
+	    VALUES("9'' Meat Cleaver", "Kitchen", 66.57, 10);
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-	    VALUES();
+	    VALUES("98'' LED Smart TV", "Electronics", 69999.99, 10);
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-	    VALUES();
+	    VALUES("Wireless Camera", "Electronics", 23.49, 30);
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-	    VALUES();
+	    VALUES("Dreadloc Tightrning Spray", "Beauty", 17.95, 80);
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-	    VALUES();
+	    VALUES("Mango & Lime Oil", "Beauty", 60.80, 30);
